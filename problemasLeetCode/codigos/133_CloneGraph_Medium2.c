e73bd7cee29d003be0164722263c670402cfac4f
DiceRunner714 <eduardo.sandes6@gmail.com>
2024-11-07 15:48:12 -0300

    isola bfs questao 133

--- problemasLeetCode/codigos/133_CloneGraph_Medium2.c
+++ problemasLeetCode/codigos/133_CloneGraph_Medium2.c
@@ -17,34 +17,36 @@ Vertice* criaVertice(int valor){
     return novoVertice;
 }
 
-Vertice* cloneGraph(Vertice* vertice){
-    if(!vertice) return NULL;
-    
+Vertice* BFSclona(Vertice* vertice){
     Vertice* verticesVisitados[MAX_NODES] = { NULL };
     
     Vertice* fila[MAX_NODES];
     int front = 0, back = 0;
-    
+
+    //Clona o nó inicial
     Vertice* cloneVertice = criaVertice(vertice->valor);
     verticesVisitados[vertice->valor] = cloneVertice;
     fila[back++] = vertice;
-    
+
+    // Enquanto a fila não está vazia
     while(front < back){
-        Vertice* verticeAtual = fila[front++];
-        
-        Vertice* cloneVerticeAtual = verticesVisitados[verticeAtual->valor];
-        
+        Vertice* verticeAtual = fila[front++]; //Desenfilera primeiro vértice na fila
+        Vertice* cloneVerticeAtual = verticesVisitados[verticeAtual->valor]; //Clona verticeAtual
+
+        //Passa por todos os vizinhos de verticeAtual
         for(int i = 0; i < verticeAtual->qtdVizinhos; i++){
             Vertice* vizinho = verticeAtual->vizinhos[i];
-            
+
+            //Se o vizinho ainda não foi visitado, ele é clonado
             if(!verticesVisitados[vizinho->valor]){
                 verticesVisitados[vizinho->valor] = criaVertice(vizinho->valor);
                 fila[back++] = vizinho;
             }
-
+            //Adiciona  a aresta
             cloneVerticeAtual->vizinhos[cloneVerticeAtual->qtdVizinhos++] = verticesVisitados[vizinho->valor];
         }
     }
-    
     return cloneVertice;
 }
+
+Vertice* cloneGraph(Vertice* vertice){ return !vertice ? NULL : BFSclona(vertice); }
